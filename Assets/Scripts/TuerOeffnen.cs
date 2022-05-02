f71d20169d292471af62eed9da630f6c44b359b8
AdLer-Lukas <95357660+AdLer-Lukas@users.noreply.github.com>
2022-05-02 13:37:04 +0200

    updated TuerOeffnen Script

--- Assets/Scripts/TuerOeffnen.cs
+++ Assets/Scripts/TuerOeffnen.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class TuerOeffnen : MonoBehaviour
 {
     public bool zuOeffnen = false;
-    private Quaternion startDrehung;
+    public bool oeffnungsRichtungUmkehren; 
+    private Quaternion startWinkel;
+    private Quaternion drehung = Quaternion.Euler(0,90,0);
 
     void Start(){
-        startDrehung = transform.rotation;
+        startWinkel = transform.rotation;
+        if(oeffnungsRichtungUmkehren){
+            drehung = Quaternion.Euler(0,-90,0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         if(zuOeffnen){
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,-90,0), Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, startWinkel*drehung, Time.deltaTime);
+        }
+
+        if (transform.rotation == startWinkel*drehung ){
+            zuOeffnen = false;
         }
         
     }
